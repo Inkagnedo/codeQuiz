a278a832ad2f465a4382fd3b6108089650d0f641
Inkagnedo <CCelestin44@gmail.com>
2020-01-16 18:54:24 -0500

    first commit

---
+++ questions.js
@@ -0,0 +1,25 @@
+const startButton = document.getElementById('start-btn', 'score-btn')
+
+
+startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', highScores)
+
+function startGame() {
+    startButton.classList.add('hide')
+}
+
+function timer() {
+
+}
+
+function highScores() {
+    startButton.classList.add('hide')
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
